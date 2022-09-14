41000af87f819ee843581327823dfe81f7fdc86c
Emre YÜCE <emreyuce90@hotmail.com>
2022-09-14 13:02:03 +0300

    material design added to project

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
 import { RouterModule } from '@angular/router';
-
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
 
 //Component modülünü Layout modülüne ekledik
 //Component modülünü dışa açar
@@ -15,7 +15,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     ComponentsModule,
-    RouterModule
+    RouterModule,
+    MatSidenavModule
   ],
   exports:[
     LayoutComponent
