2f6fb9feadcbd9209e31cd3404317bb5388c488f
Emre YÜCE <emreyuce90@hotmail.com>
2022-09-23 17:55:24 +0300

    e

--- src/app/common/models/product.service.ts
+++ src/app/common/models/product.service.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ListComponent } from 'src/app/admin/components/customers/list/list.component';
 import { ProductCreateModel } from 'src/app/contracts/ProductCreateModel';
+import { Productlist } from 'src/app/contracts/productlist';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -9,9 +12,22 @@ export class ProductService {
 
   constructor(private httpClientService: HttpClientService) { }
 
-  create(product: ProductCreateModel,successCallback:any) {
+  create(product: ProductCreateModel, successCallback: any) {
     this.httpClientService.post({
       controller: "products"
-    }, product).subscribe(result=>successCallback());
+    }, product).subscribe(result => successCallback())
+  };
+
+
+/*Bize eğer liste gelirse listeyi return edicez, eğer liste gelmezse hata mesajını göndericez*/
+
+  async list(product: Productlist, successCallback?: ()=>void, errorCallback?: (errorMessage:string)=>void) {
+    const promiseData :Promise<Productlist[]> = this.httpClientService.get<Productlist[]>({
+      controller:"products"
+    }).toPromise();
+
+    promiseData.then(d=>successCallback())
+    .catch((errorResponse:HttpErrorResponse)=>errorCallback(errorResponse.error))
+    return await promiseData;
   }
 }
