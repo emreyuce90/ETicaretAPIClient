7d0250769084fcf140eaad51360bb7809296f3e6
Emre YÜCE <emreyuce90@hotmail.com>
2022-10-31 17:22:35 +0300

    *reactive forms

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +11,46 @@ export class RegisterComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
   frm: FormGroup;
   ngOnInit(): void {
+
     this.frm = this.formBuilder.group({
-      adSoyad:[""],
-      kullaniciAdi:[""],
-      email:[""],
-      password:[""],
-      passwordConfirm:[""]
+      adSoyad: ["", [
+        Validators.required,
+        Validators.min(3),
+        Validators.max(50)
+      ]],
+      kullaniciAdi: ["", 
+      [Validators.required, 
+        Validators.min(3), 
+        Validators.max(50)
+      ]],
+      email: ["", 
+      [Validators.email, 
+        Validators.required
+      ]],
+      password: ["", [
+        Validators.required,
+         
+        ]],
+      passwordConfirm: ["", 
+      [
+        Validators.required
+      ]]
     })
   }
 
-  onSubmit(value:any){
+
+  get component() {
+    return this.frm.controls;
+  }
+
+  submitted: boolean = false;
+
+  onSubmit(value: any) {
+    this.submitted = true;
     debugger;
+    var c = this.component;
+    if (this.frm.invalid)
+      return;
   }
 
 }
